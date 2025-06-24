3371065b9285a5d7cd2cf12f26109a598ea5cd75
Aakarsh <aakarshbatra01@gmail.com>
2025-06-24 14:01:32 +0530

    Login UI complete

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss'
 })
 export class Login {
+  rightPanelActive = false;
 
+  name: string = "";
+  email: string = "";
+  password: string = "";
+  // token: string = "testToken";
+
+  // loginRequest= new Request("http://localhost:5122/api/auth/login", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     email: this.email,
+  //     password: this.password
+  //   })
+  // })
+
+  showSignUp(): void {
+    this.rightPanelActive = true;
+  }
+
+  showSignIn(): void {
+    this.rightPanelActive = false;
+  }
+
+  async activateSignIn() {
+    // const response = await fetch(this.loginRequest);
+    // this.token = response.text.toString();
+    // console.log(this.token);
+
+
+  }
 }
