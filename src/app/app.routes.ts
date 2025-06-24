3060de5224e3ec94e174dd539bf06707d1fbe3d6
Aakarsh <aakarshbatra01@gmail.com>
2025-06-24 11:38:11 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { PageNotFound } from './components/page-not-found/page-not-found';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "login", component: Login},
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  {path: "**", component: PageNotFound}
+];
